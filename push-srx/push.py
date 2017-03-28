baa42433e2f5354cd082c18375076b8decf335fa
root <root@b7s38.juniper.net>
2017-03-28 11:10:11 -0700

    push/pop config from vSRX on pod create/delete

--- push-srx/push.py
+++ push-srx/push.py
@@ -8,24 +8,41 @@ def push(host, port, user, password):
             port=port,
             username=user,
             password=password,
-            timeout=10,
+            timeout=40,
             device_params = {'name':'junos'},
             hostkey_verify=False)
 
     	firewall = conn.command(command='show configuration firewall family inet filter OpenShift', format='text').tostring
+	firewall_term_list = [firewall_term.split()[0] for firewall_term in firewall.split("term") if firewall_term.strip().startswith("frontend")]
 
-    	config, term_list = populate.fetch()
+    	config, config_term_list = populate.fetch()
 
-    	for term in term_list:
+	try:
+		delete_list = list(set(firewall_term_list) - set(config_term_list))
+		if delete_list:
+			for term_frontend in delete_list:
+				command='delete firewall family inet filter OpenShift term ' + str(term_frontend)
+				conn.load_configuration(action='set', config=command)
+				print "Deleted vSRX term {}".format(term_frontend)
+			check_config = conn.validate()
+			conn.commit()
+                        conn.close_session()
+		else:
+			pass
+	except:
+		pass
+
+    	for term in config_term_list:
 		if term in firewall:
 			pass
 		else:
     			command =[line.rstrip("\n") for line in config if line.startswith(("set","delete"))]
     			send_config = conn.load_configuration(action='set', config=command)
-    			print send_config.tostring
+			print "Pushed config to vSRX"
+#    			print send_config.tostring
 
     			check_config = conn.validate()
-    			print check_config.tostring
+#    			print check_config.tostring
 
     			conn.commit()
     			conn.close_session()
