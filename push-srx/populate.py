e37d0ad96c959a70c26ce321ffdb0a2dd1853a13
Savvy_Through <savithruml@users.noreply.github.com>
2017-03-29 15:26:28 -0700

    Update populate.py

--- push-srx/populate.py
+++ push-srx/populate.py
@@ -1,3 +1,10 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+# Author: SAVITHRU M LOKANATH
+# Contact: SAVITHRU AT JUNIPER.NET
+# Copyright (c) 2017 Juniper Networks, Inc. All rights reserved.
+
 import os, sys, json, requests, jinja2
 
 def generateTemplate(ip_address, ip_port, ip_protocol, term_name):
